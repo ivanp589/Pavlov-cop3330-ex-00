7b0bf2d799cb5a28f1e00b095d9522f360e787aa
ivanp589 <ivanpav@Knights.ucf.edu>
2021-09-06 11:59:43 -0400

    final update working..

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
-  Hello world!
- *
+ /*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ivan Pavlov
  */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "\n Hello3 World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\n what is your name? ");
+        String name = scanner.next();
 
+        //      System.out.println(name);
+        System.out.print("\n Hello, ");
+        System.out.print(name);
+        System.out.print(", " + "nice to meet you!");
     }
 }
